80b33c70a602b181a9e6642e58a9ed312c0b47aa
cursos <cursos@host.docker.internal>
2024-06-03 12:03:35 +0200

    precarga datos

---
+++ web1/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+DELETE from Alumno;
+INSERT INTO Alumno (nombre, edad) VALUES ('Juan', 25);
+INSERT INTO Alumno (nombre, edad) VALUES ('Maria', 30);
+INSERT INTO alumno (nombre, edad) VALUES ('Carlos', 22);
---
+++ web1/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS Alumno;
+CREATE TABLE Alumno (
+ 
+    nombre VARCHAR(255) NOT NULL PRIMARY KEY,
+    edad INT NOT NULL
+);
