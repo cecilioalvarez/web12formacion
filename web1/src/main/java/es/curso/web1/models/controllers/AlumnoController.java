157dbb597d08de924c2e8ba4528c388bdc6e0186
cursos <cursos@host.docker.internal>
2024-06-03 13:23:32 +0200

    buscarExamenporNombre

--- web1/src/main/java/es/curso/web1/models/controllers/AlumnoController.java
+++ web1/src/main/java/es/curso/web1/models/controllers/AlumnoController.java
@@ -1,21 +1,26 @@
 package es.curso.web1.models.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.web1.models.Alumno;
+import es.curso.web1.models.Examen;
 import es.curso.web1.models.repositories.AlumnoRepository;
+import es.curso.web1.models.repositories.ExamenRepository;
 
 @Controller
 public class AlumnoController {
 
 	@Autowired
 	AlumnoRepository alumnoRepository;
+	
+	@Autowired
+	ExamenRepository examenRepository;
 	
 	//una url  que se encarga de esto
 	// y nos redirige a listaalumnos.xhtml
@@ -27,6 +32,18 @@ public class AlumnoController {
 		modelo.addAttribute("lista", lista);
 		
 		return "listaalumnos.xhtml";
+		
+	}
+	
+	
+	@GetMapping("/verexamenes")
+	public String verExamenes(Model modelo, @RequestParam String nombre) {
+		List<Examen> lista=examenRepository.buscarExamenPorNombreAlumno(nombre);
+		
+		//asigna a la plantilla una nombre de lista junto con los datos
+		modelo.addAttribute("lista", lista);
+		
+		return "verexamenes.xhtml";
 		
 	}
 }
