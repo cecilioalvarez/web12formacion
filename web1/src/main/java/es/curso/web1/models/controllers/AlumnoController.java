4059977ec2fee46e4808a104fe40392013d20665
cursos <cursos@host.docker.internal>
2024-06-03 12:19:08 +0200

    jpa con lista de alumnos

--- web1/src/main/java/es/curso/web1/models/controllers/AlumnoController.java
+++ web1/src/main/java/es/curso/web1/models/controllers/AlumnoController.java
@@ -3,22 +3,26 @@ package es.curso.web1.models.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import es.curso.web1.models.Alumno;
+import es.curso.web1.models.repositories.AlumnoRepository;
 
 @Controller
 public class AlumnoController {
 
+	@Autowired
+	AlumnoRepository alumnoRepository;
+	
 	//una url  que se encarga de esto
 	// y nos redirige a listaalumnos.xhtml
 	@GetMapping("/listaalumnos")
 	public String listaAlumnos(Model modelo) {
-		List<Alumno> lista= new ArrayList<Alumno>();
-		lista.add(new Alumno("pepe",20));
-		lista.add(new Alumno("ana",30));
+		List<Alumno> lista=alumnoRepository.buscarTodos();
+		
 		//asigna a la plantilla una nombre de lista junto con los datos
 		modelo.addAttribute("lista", lista);
 		
