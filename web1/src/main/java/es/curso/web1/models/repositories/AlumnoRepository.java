82879a9ad2cc236c6d615a19d74ed0d4cffb863c
cursos <cursos@host.docker.internal>
2024-06-03 12:15:55 +0200

    alumno

--- web1/src/main/java/es/curso/web1/models/repositories/AlumnoRepository.java
+++ web1/src/main/java/es/curso/web1/models/repositories/AlumnoRepository.java
@@ -1,12 +1,23 @@
 package es.curso.web1.models.repositories;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import es.curso.web1.models.Alumno;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 @Repository
 public class AlumnoRepository {
 
 	@PersistenceContext
 	EntityManager em;
+	
+	public List<Alumno> buscarTodos() {
+		//java persistence query language
+		TypedQuery<Alumno> consulta= em.createQuery("select a from Alumno a"
+				,Alumno.class);
+		return consulta.getResultList();
+	}
 }
