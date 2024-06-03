94697ea9eb802f5f7a99c14283095d0d6afca881
cursos <cursos@host.docker.internal>
2024-06-03 11:06:00 +0200

    nuevo

---
+++ web1/src/main/java/es/curso/web1/models/Alumno.java
@@ -0,0 +1,26 @@
+package es.curso.web1.models;
+
+public class Alumno {
+
+	private String nombre;
+	private int edad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public Alumno(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	
+}
