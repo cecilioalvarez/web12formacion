0107e2ee63b82e3d21758acf85f7b133c111b779
cursos <cursos@host.docker.internal>
2024-06-04 09:21:02 +0200

    formulario e inserciones

--- web1/src/main/java/es/curso/web1/models/controllers/AlumnoController.java
+++ web1/src/main/java/es/curso/web1/models/controllers/AlumnoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.web1.models.Alumno;
@@ -18,53 +20,65 @@ public class AlumnoController {
 
 	@Autowired
 	AlumnoRepository alumnoRepository;
-	
+
 	@Autowired
 	ExamenRepository examenRepository;
-	
-	//una url  que se encarga de esto
+
+	// una url que se encarga de esto
 	// y nos redirige a listaalumnos.xhtml
 	@GetMapping("/listaalumnos")
 	public String listaAlumnos(Model modelo) {
-		List<Alumno> lista=alumnoRepository.buscarTodos();
-		
-		//asigna a la plantilla una nombre de lista junto con los datos
+		List<Alumno> lista = alumnoRepository.buscarTodos();
+
+		// asigna a la plantilla una nombre de lista junto con los datos
 		modelo.addAttribute("lista", lista);
-		
+
 		return "listaalumnos.xhtml";
-		
+
 	}
-	
-	
+
 	// redireccion
 	@GetMapping("/formularioalumno")
 	public String formularioAlumno() {
-		
+
 		return "formularioalumno.xhtml";
-		
+
 	}
-	
-	
-	
-	
+	// model attribute mapea el formulario con sus datos
+	// aun objeto
+
+	@PostMapping("/insertaralumno")
+	public String formularioAlumno(@ModelAttribute Alumno alumno, Model modelo) {
+
+		alumnoRepository.insertar(alumno);
+
+		List<Alumno> lista = alumnoRepository.buscarTodos();
+
+		// asigna a la plantilla una nombre de lista junto con los datos
+		modelo.addAttribute("lista", lista);
+		return "listaAlumnos.xhtml";
+
+	}
+
 	@GetMapping("/verexamenes")
 	public String verExamenes(Model modelo, @RequestParam String nombre) {
-		List<Examen> lista=examenRepository.buscarExamenPorNombreAlumno(nombre);
-		
-		//asigna a la plantilla una nombre de lista junto con los datos
+		List<Examen> lista = examenRepository.buscarExamenPorNombreAlumno(nombre);
+
+		// asigna a la plantilla una nombre de lista junto con los datos
 		modelo.addAttribute("lista", lista);
-		
+
 		return "verexamenes.xhtml";
-		
+
 	}
+
 	@GetMapping("/verexamenestotal")
 	public String verExamenesTotal(Model modelo) {
-		List<Examen> lista=examenRepository.buscarExamenes();
-		
-		//asigna a la plantilla una nombre de lista junto con los datos
+		List<Examen> lista = examenRepository.buscarExamenes();
+
+		// asigna a la plantilla una nombre de lista junto con los datos
 		modelo.addAttribute("lista", lista);
-		
+
 		return "verexamenestotal.xhtml";
-		
+
 	}
 }
